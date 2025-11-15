b9b6fbcc222a6ddc0118fafdfc6c2d32c5c7e020
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-15 17:04:10 +0100

    Initial Commit

---
+++ Dungeon Dice/Dungeon_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dungeon_DiceApp.swift
+//  Dungeon Dice
+//
+//  Created by Cem Aksoy on 15.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Dungeon_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
